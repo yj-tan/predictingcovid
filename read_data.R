9a72d09291e91d70bb2357fb9896f04a07bc3298
Victor Song <vms2@rice.edu>
2020-11-02 19:44:49 -0600

    Joined mask and mobility data

--- read_data.R
+++ read_data.R
@@ -4,7 +4,10 @@ MASK_USE_PATH <- "mask-use-by-county.csv"
 MOBILITY_PATH <- "2020_US_Region_Mobility_Report.csv"
 PROTEST_PATH <- "protests.csv"
 
-mask_use <- read.csv(MASK_USE_PATH, colClasses = c("COUNTYFP" = "factor"))
+mask_use <- read.csv(MASK_USE_PATH,
+    colClasses = c("COUNTYFP" = "factor")
+) %>%
+    as_tibble()
 
 mobility <- read.csv(MOBILITY_PATH,
     colClasses = c(
@@ -16,9 +19,13 @@ mobility <- read.csv(MOBILITY_PATH,
         "census_fips_code" = "factor",
         "date" = "Date"
     )
-)
+) %>%
+    as_tibble()
 
 mobility %<>%
+    filter(census_fips_code != ""
+    & date >= "2020-07-21"
+    & date <= "2020-07-27") %>%
     group_by(census_fips_code) %>%
     select(-c(
         country_region_code,
@@ -29,13 +36,15 @@ mobility %<>%
         iso_3166_2_code,
         date
     )) %>%
-    summarise(across(.fns = ~ mean(na.omit(.x), na.rm = TRUE)))
+    summarise(across(.fns = ~ mean(.x, na.rm = TRUE)))
+
+both_mask_mobility <- left_join(mobility, mask_use, by = c("census_fips_code" = "COUNTYFP"))
 
 protests <- read.csv(PROTEST_PATH, colClasses = c("Date" = "Date"))
 protests <- filter(
     protests,
     (Event..legacy..see.tags. == "Racial Injustice"
     | Event..legacy..see.tags. == "Civil Rights")
-    & Date >= "2020-03-13"
-    & Date <= "2020-10-20"
+    & Date >= "2020-07-21"
+    & Date <= "2020-07-27"
 )
