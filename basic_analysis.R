38cd7308a878cf76623fed8f363a677d4acf157b
Victor Song <vms2@rice.edu>
2020-11-08 16:56:16 -0600

    Added code for BAS

--- basic_analysis.R
+++ basic_analysis.R
@@ -1,11 +1,14 @@
 library("mgcv")
+library("BAS")
 library("tidyverse")
 
 LOG_RDS_CASES_PATH <- "final_logcases_dat.rds"
+N_MODELS <- 50000
 
 dataset <-
     readRDS(LOG_RDS_CASES_PATH) %>%
     as_tibble() %>%
+    mutate(log_population_density_per_sqmi = log(population_density_per_sqmi)) %>%
     mutate(across(!starts_with("log_mean") & where(is.numeric), scale))
 
 basic_gam <- gam(
@@ -22,3 +25,37 @@ basic_gam <- gam(
 )
 
 summary(basic_gam)
+
+basic_bas <- bas.lm(
+    formula = log_mean_confirmed_7d_total ~ . - population_density_per_sqmi,
+    data = dataset,
+    method = "MCMC",
+    prior = "JZS",
+    update = 100,
+    n.models = N_MODELS
+)
+
+poly_bas <- bas.lm(
+    formula = log_mean_confirmed_7d_total ~ poly(never_rarely_mask, 3)
+    + poly(retail_and_recreation_percent_change_from_baseline, 3)
+        + poly(workplaces_percent_change_from_baseline, 3)
+        + stay_at_home + log_population_density_per_sqmi
+        + poly(percent_uninsured, 3) + poly(percent_non_hispanic_white, 3)
+        + poly(per_capita_income, 3) + poly(percent_65_and_over, 3)
+        + poly(ELEV_M, 3) + poly(mean_temp_15d_avg),
+    data = dataset,
+    method = "MCMC",
+    prior = "JZS",
+    update = 100,
+    n.models = N_MODELS
+)
+
+gprior_bas <- bas.lm(
+    formula = log_mean_confirmed_7d_total ~ . - population_density_per_sqmi,
+    data = dataset,
+    method = "MCMC",
+    prior = "g-prior",
+    alpha = 4,
+    update = 100,
+    n.models = N_MODELS
+)
