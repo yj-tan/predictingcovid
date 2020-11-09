41baa9eb0ef8059e1a7cca8e1987806c3f3992b7
Jason Tan <jason@jasons-mbp.dyndns.rice.edu>
2020-11-09 02:40:03 -0600

    diff code

--- analysis.R
+++ analysis.R
@@ -2,10 +2,18 @@ library("tidyverse")
 library("BAS")
 
 LOG_RDS_CASES_PATH <- "final_logcases_dat.rds"
-N_MODELS <- 50000
+LOG_RDS_DIFFCASES_PATH <- "final_logdiffcases_dat.rds"
+N_MODELS <- 10000
 
 dataset <-
     readRDS(LOG_RDS_CASES_PATH) %>%
+    as_tibble() %>%
+    mutate(log_population_density_per_sqmi = log(population_density_per_sqmi)) %>%
+    select(-population_density_per_sqmi) %>%
+    mutate(across(!starts_with("log_mean") & where(is.numeric), scale))
+
+diff_dataset <-
+    readRDS(LOG_RDS_DIFFCASES_PATH) %>%
     as_tibble() %>%
     mutate(log_population_density_per_sqmi = log(population_density_per_sqmi)) %>%
     select(-population_density_per_sqmi) %>%
@@ -31,6 +39,30 @@ inter_1o_bas <- bas.lm(
     n.models = N_MODELS,
     bigmem = T
 )
+
+diff_inter_1o_bas <- bas.lm(
+    formula = log_mean_confirmed_7d_diff ~ poly(never_rarely_mask, 3)
+    + poly(retail_and_recreation_percent_change_from_baseline, 3)
+    + poly(workplaces_percent_change_from_baseline, 3)
+    + stay_at_home
+    + log_population_density_per_sqmi
+    + poly(percent_uninsured, 3)
+    + poly(percent_non_hispanic_white, 3)
+    + poly(per_capita_income, 3)
+    + poly(percent_65_and_over, 3)
+    + poly(ELEV_M, 3)
+    + poly(mean_temp_15d_avg)
+    + ((.)^2 - .),
+    data = diff_dataset,
+    method = "MCMC",
+    prior = "JZS",
+    update = 100,
+    n.models = N_MODELS,
+    bigmem = T
+)
+
+diff_lmtest <- lm(log_mean_confirmed_7d_diff ~ ., diff_dataset)
+
 
 smaller_bas <- bas.lm(
     formula =
@@ -62,3 +94,10 @@ plot(coef_smaller_bas)
 plot(confint(coef_smaller_bas))
 
 diagnostics(coef_smaller_bas)
+
+# posterior predictive checks
+BPM <- predict(smaller_bas, estimator = "BPM", se.fit = TRUE)
+conf.fit <- confint(BPM, parm = "mean")
+conf.pred <- confint(BPM, parm = "pred")
+plot(conf.fit)
+plot(conf.pred)
